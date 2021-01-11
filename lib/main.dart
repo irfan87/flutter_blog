2d38066518a2e326c6496da7b151746ef1248c71
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-01-11 21:46:42 +0800

    button onPressed and callback between two classes

--- lib/main.dart
+++ lib/main.dart
@@ -19,26 +19,47 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomepage extends StatelessWidget {
+class MyHomepage extends StatefulWidget {
+  @override
+  _MyHomepageState createState() => _MyHomepageState();
+}
+
+class _MyHomepageState extends State<MyHomepage> {
+  String text = '';
+
+  void changedText(String text) {
+    this.setState(() {
+      this.text = text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Blog'),
       ),
-      body: TextInputWidget(),
+      body: Column(
+        children: [
+          TextInputWidget(this.changedText),
+          Text(this.text),
+        ],
+      ),
     );
   }
 }
 
 class TextInputWidget extends StatefulWidget {
+  final Function(String) callback;
+
+  TextInputWidget(this.callback);
+
   @override
   _TextInputWidgetState createState() => _TextInputWidgetState();
 }
 
 class _TextInputWidgetState extends State<TextInputWidget> {
   final textEditingController = TextEditingController();
-  String text = '';
 
   // will wipe the current data from textEditingController
   @override
@@ -49,31 +70,28 @@ class _TextInputWidgetState extends State<TextInputWidget> {
     textEditingController.dispose();
   }
 
-  void changedText(text) {
-    if (text == 'test') {
-      textEditingController.clear();
-      text = '';
-    }
-    setState(() {
-      this.text = text;
-    });
+  void onClick() {
+    widget.callback(textEditingController.text);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextField(
-          onChanged: (text) => this.changedText(text),
-          controller: this.textEditingController,
-          decoration: InputDecoration(
-              prefixIcon: Icon(
-                Icons.message,
-              ),
-              labelText: 'Type a message'),
+    return TextField(
+      controller: this.textEditingController,
+      decoration: InputDecoration(
+        prefixIcon: Icon(
+          Icons.message,
         ),
-        Text(this.text),
-      ],
+        labelText: 'Type a message',
+        suffixIcon: IconButton(
+          onPressed: this.onClick,
+          splashColor: Colors.amber[200],
+          tooltip: 'Post Message',
+          icon: Icon(
+            Icons.send,
+          ),
+        ),
+      ),
     );
   }
 }
